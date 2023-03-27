5e640a8f6a8c5cf77a0d2320942fedcd2b0219ea
Shuuyaacreat <124362054+Shuuyaacreat@users.noreply.github.com>
2023-03-28 01:51:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+SESSION_NAME = "BQCa8HfFT9fAbNJLg7uq0BqMNqDM8dceBXlLIzr7S-Ss8JH2EMqABHhljommV67MhSh0mrVE-TWEg3sIkhC8fg6Wgvjx1CopYCogNyvg3y3UoEUbVEQBitoV-dkw2QPgOSIRKgJ8GaTUCqXust5b5Hs2d_BiH_rUcCIpKfRHiB-q6U_r8TM92fV1Pxh9GwfkpqIlZmpJr_Y6-iZqv6IUiuL5V6RNSSfkV01yvf_qsj2EKA5EsfWandC6C4e81IcydgGnz5Rf3egX-lPj7QGn8XsXJN_-cAlohl_orwypeuohlpagDAk_X3hDyKTBEjqoYan0_nIGYuSXv14HipwkQjSVAAAAAWiq_DMA" 
+
+BOT_TOKEN = "6261340376:AAF-ClLZcHnJ-BcHy5k25BdY3UOWOfL_Vvc" # Get a token from @botFather
+
+API_ID = "24427150" # Get this value from https://my.telegram.org
+
+API_HASH = "9fcc60263a946ef550d11406667404fa" #  Get this value from https://my.telegram.org
+
+SUDO_USERS = "" # For multiple Users Use Space
+
+DURATION_LIMIT = "60" # In Mins (EX :- 60)
+
+ASSISTANT_NAME = "Shuyaahunmai" # Fill there Your Assistant Username without @
+
+BOT_USERNAME = "Zenizko_bot" # fill there Your Bot Username Without @
+
+UPDATES_CHANNEL = "" # Support Channel Link if you have any, Leave blank if no support. Your Username must start without @
+
+GROUP_SUPPORT = "" #Support Group Link if you have any, Leave blank if no support. Your Username must start without @
