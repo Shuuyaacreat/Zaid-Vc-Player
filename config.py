e6945808dd0b34741b3314504725eb61f70beac1
Shuuyaacreat <124362054+Shuuyaacreat@users.noreply.github.com>
2023-03-28 01:31:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ if os.path.exists("local.env"):
 load_dotenv()
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME")
+SESSION_NAME = getenv("shuyaa")
 
-if str(getenv("STRING_SESSION2")).strip() == "":
+if str(getenv("BQCa8HfFT9fAbNJLg7uq0BqMNqDM8dceBXlLIzr7S-Ss8JH2EMqABHhljommV67MhSh0mrVE-TWEg3sIkhC8fg6Wgvjx1CopYCogNyvg3y3UoEUbVEQBitoV-dkw2QPgOSIRKgJ8GaTUCqXust5b5Hs2d_BiH_rUcCIpKfRHiB-q6U_r8TM92fV1Pxh9GwfkpqIlZmpJr_Y6-iZqv6IUiuL5V6RNSSfkV01yvf_qsj2EKA5EsfWandC6C4e81IcydgGnz5Rf3egX-lPj7QGn8XsXJN_-cAlohl_orwypeuohlpagDAk_X3hDyKTBEjqoYan0_nIGYuSXv14HipwkQjSVAAAAAWiq_DMA")).strip() == "BQCa8HfFT9fAbNJLg7uq0BqMNqDM8dceBXlLIzr7S-Ss8JH2EMqABHhljommV67MhSh0mrVE-TWEg3sIkhC8fg6Wgvjx1CopYCogNyvg3y3UoEUbVEQBitoV-dkw2QPgOSIRKgJ8GaTUCqXust5b5Hs2d_BiH_rUcCIpKfRHiB-q6U_r8TM92fV1Pxh9GwfkpqIlZmpJr_Y6-iZqv6IUiuL5V6RNSSfkV01yvf_qsj2EKA5EsfWandC6C4e81IcydgGnz5Rf3egX-lPj7QGn8XsXJN_-cAlohl_orwypeuohlpagDAk_X3hDyKTBEjqoYan0_nIGYuSXv14HipwkQjSVAAAAAWiq_DMA":
     SESSION2 = str(None)
 else:
     SESSION2 = str(getenv("STRING_SESSION2"))
@@ -33,28 +33,28 @@ if str(getenv("STRING_SESSION5")).strip() == "":
 else:
     SESSION5 = str(getenv("STRING_SESSION5"))
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_NAME = getenv("BOT_NAME", "Umk")
+BOT_TOKEN = getenv("BOT_TOKEN", "6261340376:AAF-ClLZcHnJ-BcHy5k25BdY3UOWOfL_Vvc")
+BOT_NAME = getenv("BOT_NAME", "Zenistu")
 
-API_ID = int(getenv("API_ID", "8186557"))
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
+API_ID = int(getenv("API_ID", "24427150"))
+API_HASH = getenv("API_HASH", "9fcc60263a946ef550d11406667404fa")
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Cloner:Cloner@cluster0.cgc6t.mongodb.net/?retryWrites=true&w=majority")
-OWNER_NAME = getenv("OWNER_NAME", "Zaid")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Timesisnotwaiting")
-ALIVE_NAME = getenv("ALIVE_NAME", "Zaid")
+OWNER_NAME = getenv("OWNER_NAME", "Shuyaaa")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "@Shuyaahunmai")
+ALIVE_NAME = getenv("ALIVE_NAME", "Zenistu")
 BOT_USERNAME = getenv("BOT_USERNAME", "Zaid2_Robot")
 OWNER_ID = getenv("OWNER_ID", "1669178360")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Zaid2_Assistant")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "TheSupportChat")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "TheUpdatesChannel")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Zenizko_bot")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Spoiled_Community")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-HEROKU_API_KEY = getenv("UPDATES_CHANNEL", "HEROKU_API_KEY")
+HEROKU_API_KEY = getenv("UPDATES_CHANNEL", "415484ae-e0d1-481b-a9d7-7ef55c2e549d")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/fc9d87ffd1c6f828eb7fc.png")
 START_PIC = getenv("START_PIC", "https://telegra.ph/file/a414e2cdfeaa7d4414b89.png")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ITZ-ZAID/Zaid-Vc-Player")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO"")
 PLAY_IMG = getenv("PLAY_IMG", "https://telegra.ph/file/10b1f781170b1e1867f68.png")
 QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/b95c13eef1ebd14dbb458.png")
 CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
